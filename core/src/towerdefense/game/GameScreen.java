d60c878bedbac513e02ec7e7f3c458f952c5ef7b
Izabela Ryś <cytrynowa.rys@gmail.com>
2020-08-31 17:19:11 +0200

    Initial game setup.

---
+++ core/src/towerdefense/game/GameScreen.java
@@ -0,0 +1,73 @@
+package towerdefense.game;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class GameScreen implements Screen {
+
+    //screen
+    private Camera camera;
+    private Viewport viewport;
+
+    //graphics
+    private SpriteBatch batch;
+    private Texture background;
+
+    //world parameters
+    private final int WORLD_WIDTH = 512;
+    private final int WORLD_HEIGHT = 128;
+
+    GameScreen() {
+        camera = new OrthographicCamera();
+        viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+
+        background = new Texture("background.jpg");
+
+        batch = new SpriteBatch();
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        batch.begin();
+
+        batch.draw(background,0,0,WORLD_WIDTH,WORLD_HEIGHT);
+
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width,height,true);
+        batch.setProjectionMatrix(camera.combined);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
