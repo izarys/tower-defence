c912574160e250e48c4beb89203674f8ae85e12e
Izabela Ryś <cytrynowa.rys@gmail.com>
2020-09-03 12:08:24 +0200

    Adding TextField.

--- core/src/towerdefense/game/GameScreen.java
+++ core/src/towerdefense/game/GameScreen.java
@@ -1,12 +1,18 @@
 package towerdefense.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.util.LinkedList;
 
 public class GameScreen implements Screen {
 
@@ -22,13 +28,39 @@ public class GameScreen implements Screen {
     private final int WORLD_WIDTH = 512;
     private final int WORLD_HEIGHT = 128;
 
+    Stage stage;
+    Skin skin;
+    TextField textField;
+
     GameScreen() {
         camera = new OrthographicCamera();
         viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
 
+        //setting up the textures
         background = new Texture("background.jpg");
 
         batch = new SpriteBatch();
+
+        //setting up textField
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        textField = new TextField("", skin);
+        textField.setMessageText("type here ...");
+        textField.setPosition(180,20);
+        textField.setSize(300,50);
+
+        textField.setTextFieldListener(new TextField.TextFieldListener() {
+            @Override
+            public void keyTyped(TextField textField, char c) {
+                if(c == 13) { // the typed key is ENTER
+                    System.out.println(textField.getText());
+                    textField.setText("");
+                }
+            }
+        });
+        stage.addActor(textField);
     }
 
     @Override
@@ -43,6 +75,9 @@ public class GameScreen implements Screen {
         batch.draw(background,0,0,WORLD_WIDTH,WORLD_HEIGHT);
 
         batch.end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
