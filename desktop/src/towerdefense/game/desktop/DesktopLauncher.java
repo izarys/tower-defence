6953963cb68954828f8841dc4017c4f47c9ea831
Izabela Ryś <cytrynowa.rys@gmail.com>
2020-09-10 19:01:51 +0200

    Changed display window size.

--- desktop/src/towerdefense/game/desktop/DesktopLauncher.java
+++ desktop/src/towerdefense/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import towerdefense.game.TowerDefenseGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.height = 756;
+		config.width = 1344;
+
 		new LwjglApplication(new TowerDefenseGame(), config);
 	}
 }
